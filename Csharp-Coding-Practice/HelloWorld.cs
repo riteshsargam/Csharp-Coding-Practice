aa28a7bf76e4ccbb61e1c7353335703f373c781e
Ritesh <ritesh.sargam00@gmail.com>
2025-04-01 22:14:54 +0530

    Hello World Program

---
+++ Csharp-Coding-Practice/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice 
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
