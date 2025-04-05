8a1fa11a35e4156ee970b92051688e1146607802
Ritesh <ritesh.sargam00@gmail.com>
2025-04-05 22:27:07 +0530

    InOutParameters

---
+++ Csharp-Coding-Practice/InOutParameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class InOutParameters
+    {
+        public void Factorial(ref uint a)
+        {
+            if (a == 0 || a == 1)
+            {
+                a = 1;
+            }
+            else
+            {
+                uint result = 1;
+                for (uint i = 2; i <= a; i++)
+                {
+                    result = result * i;
+                }
+                a = result;
+            }
+        }
+        static void Main()
+        {
+            InOutParameters obj = new InOutParameters();
+            uint f = 5;
+            Console.WriteLine("Value of f before execution of the method: " + f);
+            obj.Factorial(ref f);
+            Console.WriteLine("Value of f after execution of the method: " + f);
+            Console.ReadLine();
+        }
+
+    }
+}
