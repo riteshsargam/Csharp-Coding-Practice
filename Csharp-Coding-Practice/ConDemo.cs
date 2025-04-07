75e4fdffbc03d7170cdef3be100036854262acac
Ritesh <ritesh.sargam00@gmail.com>
2025-04-07 20:03:40 +0530

    ConDemo

---
+++ Csharp-Coding-Practice/ConDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class ConDemo
+    {
+        public ConDemo()    //Explicit Constructor
+        {
+            Console.WriteLine("Constructor is called.");
+        }
+        public void Demo()  //Method
+        {
+            Console.WriteLine("Method is called.");
+        }
+        static void Main()
+        {
+            ConDemo cd1 = new ConDemo();
+            ConDemo cd2 = new ConDemo();
+            ConDemo cd3 = cd2;
+            cd1.Demo();
+            cd2.Demo();
+            cd3.Demo();
+            Console.ReadLine();
+        }
+
+    }
+}
