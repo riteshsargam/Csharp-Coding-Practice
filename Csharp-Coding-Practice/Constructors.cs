80e171763b7835e2f45c1bd83006178b6f3ad120
Ritesh <ritesh.sargam00@gmail.com>
2025-04-10 22:37:33 +0530

    Constructors

---
+++ Csharp-Coding-Practice/Constructors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class Constructors
+    {
+        static Constructors()
+        {
+            Console.WriteLine("Static constructor is called.");
+        }
+        Constructors()
+        {
+            Console.WriteLine("Non-static constructor is called.");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Main method is called.");
+            Constructors c1 = new Constructors();
+            Constructors c2 = new Constructors();
+            Constructors c3 = new Constructors();
+            Console.ReadLine();
+        }
+    }
+}
