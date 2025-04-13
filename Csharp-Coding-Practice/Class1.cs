40c9587db591c8885eafbca0111320e549936cda
Ritesh <ritesh.sargam00@gmail.com>
2025-04-13 23:33:38 +0530

    Inheritance

---
+++ Csharp-Coding-Practice/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Class1 constructor is called.");
+        }
+        public void Test1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method 2");
+        }
+
+    }
+}
---
+++ Csharp-Coding-Practice/Class2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class Class2 : Class1
+    {
+        public Class2()
+        {
+            Console.WriteLine("Class2 constructor is called.");
+        }
+        public void Test3()
+        {
+            Console.WriteLine("Method 3");
+        }
+        public void Test4()
+        {
+            Console.WriteLine("Method 4");
+        }
+        static void Main()
+        {
+            Class2 c = new Class2();
+            c.Test1(); c.Test2();   //Calling members of parent class
+            c.Test3(); c.Test4();   //Calling members of current class
+            Console.ReadLine();
+        }
+
+    }
+}
