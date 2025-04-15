2e9494fa1b8c75b8c9dc76c9d1ca856446b9d9e7
Ritesh <ritesh.sargam00@gmail.com>
2025-04-15 23:55:57 +0530

    Method Overloading

---
+++ Csharp-Coding-Practice/OverloadMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class OverloadMethods
+    {
+        public void Show()
+        {
+            Console.WriteLine(1);
+        }
+        public void Show(int i)
+        {
+            Console.WriteLine(2);
+        }
+        public void Show(string s)
+        {
+            Console.WriteLine(3);
+        }
+        public void Show(int i, string s)
+        {
+            Console.WriteLine(4);
+        }
+        public void Show(string s, int i)
+        {
+            Console.WriteLine(5);
+        }
+        static void Main()
+        {
+            OverloadMethods obj = new OverloadMethods();
+            obj.Show();
+            obj.Show(10);
+            obj.Show("Hello");
+            obj.Show(10, "Hello");
+            obj.Show("Hello", 10);
+            Console.ReadLine();
+        }
+    }
+}
