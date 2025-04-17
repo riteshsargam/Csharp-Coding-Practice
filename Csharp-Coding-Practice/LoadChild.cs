36f7c95188180b542dbc0a820e19693123d5c68b
Ritesh <ritesh.sargam00@gmail.com>
2025-04-17 19:22:46 +0530

    Inheritance-based method overloading

---
+++ Csharp-Coding-Practice/LoadChild.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class LoadChild: LoadParent
+    {
+        //Overloading parent's Test method in child
+        public void Test(int i)
+        {
+            Console.WriteLine("Child Class Test Method Is Called.");
+        }
+        static void Main()
+        {
+            LoadChild c = new LoadChild();
+            c.Test();       //Executes parent class Test method
+            c.Test(10);     //Executes child class Test method
+            c.Show();           //Executes parent class Show method
+            c.Display();    //Executes parent class Display method 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/LoadParent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class LoadParent
+    {
+        public void Test()
+        {
+            Console.WriteLine("Parent Class Test Method Is Called.");
+        }
+        public virtual void Show()  //Overridable
+        {
+            Console.WriteLine("Parent Class Show Method Is Called.");
+        }
+        public void Display()
+        {
+            Console.WriteLine("Parent Class Display Method Is Called.");
+        }
+    }
+}
