8f389f0bbff37f32e4acc43804719fb3edabfc3d
Ritesh <ritesh.sargam00@gmail.com>
2025-04-19 20:28:13 +0530

    “Operator Overloading”, “Method Overriding” and “Hiding/Shadowing”

---
+++ Csharp-Coding-Practice/Matrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class Matrix
+    {
+        //Declaring attributes for a 2 * 2 Matrix
+        int a, b, c, d;
+        //Initializing attributes of the Matrix in constructor
+        public Matrix(int a, int b, int c, int d)
+        {
+            this.a = a; this.b = b; this.c = c; this.d = d;
+        }
+        //Overriding the ToString() method inherited from Object class to return values of the Matrix in 2 * 2 format
+        public override string ToString()
+        {
+            return a + "  " + b + "\n" + c + "  " + d + "\n";
+        }
+        //Implementing the + operator so that it can be used between 2 Matrix operands
+        public static Matrix operator +(Matrix obj1, Matrix obj2)
+        {
+            Matrix obj = new Matrix(obj1.a + obj2.a, obj1.b + obj2.b, obj1.c + obj2.c, obj1.d + obj2.d);
+            return obj;
+        }
+        //Implementing the - operator so that it can be used between 2 Matrix operands
+        public static Matrix operator -(Matrix obj1, Matrix obj2)
+        {
+            Matrix obj = new Matrix(obj1.a - obj2.a, obj1.b - obj2.b, obj1.c - obj2.c, obj1.d - obj2.d);
+            return obj;
+        }
+        //Re-Implementing the == operator using Hiding/Shadowing so that it can be used between 2 Matrix’s to perform values == comparison because original implementation is reference equal comparison
+        public static bool operator ==(Matrix obj1, Matrix obj2)
+        {
+            if (obj1.a == obj2.a && obj1.b == obj2.b && obj1.c == obj2.c && obj1.d == obj2.d)
+                return true;
+            else
+                return false;
+        }
+        //Re-Implementing the != operator using Hiding/Shadowing so that it can be used between 2 Matrix’s to perform values != comparison because original implementation is reference not equal comparison
+        public static bool operator !=(Matrix obj2, Matrix obj1)
+        {
+            if (obj1.a != obj2.a || obj1.b != obj2.b || obj1.c != obj2.c || obj1.d != obj2.d)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/TestMatrix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class TestMatrix
+    {
+        static void Main()
+        {
+            //Creating 4 instances of Matrix class with different values
+            Matrix m1 = new Matrix(20, 19, 18, 17);
+            Matrix m2 = new Matrix(15, 14, 13, 12);
+            Matrix m3 = new Matrix(10, 9, 8, 7);
+            Matrix m4 = new Matrix(5, 4, 3, 2);
+            //Performing Matrix Arithmatic
+            Matrix m5 = m1 + m2 + m3 + m4;
+            Matrix m6 = m1 - m2 - m3 - m4;
+            //Printing values of each Matrix:
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine(m3);
+            Console.WriteLine(m4);
+            Console.WriteLine(m5);
+            Console.WriteLine(m6);
+
+            //Performing Matrix equal comparision
+            if (m1 == m2)
+                Console.WriteLine("Yes, m1 is equal to m2.");
+            else
+                Console.WriteLine("No, m1 is not equal to m2.");
+            //Performing Matrix not equal comparision
+            if (m1 != m2)
+                Console.WriteLine("Yes, m1 is not equal to m2.");
+            else
+                Console.WriteLine("No, m1 is equal to m2.");
+            Console.ReadLine();
+        }
+    }
+}
