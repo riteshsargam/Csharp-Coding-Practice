a6fe518b028a5438e8b5bba1123ae71176515136
Ritesh <ritesh.sargam00@gmail.com>
2025-04-22 19:21:03 +0530

    Abstract Classes and Abstract Methods

---
+++ Csharp-Coding-Practice/AbsChild.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class AbsChild : AbsParent
+    {
+        public override void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public override void Div(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+        public void Mod(int a, int b)
+        {
+            Console.WriteLine(a % b);
+        }
+        static void Main()
+        {
+            AbsChild c = new AbsChild();
+            c.Add(100, 50); c.Add(75, 17);
+            c.Mul(12, 13); c.Div(870, 15);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/AbsParent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public abstract class AbsParent
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public abstract void Mul(int a, int b);
+        public abstract void Div(int a, int b);
+    }
+}
