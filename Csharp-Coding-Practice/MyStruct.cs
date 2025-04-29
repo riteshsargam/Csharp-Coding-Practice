8d2efd574bba4f00f8686e5aabb51c1c5ce3e6e4
Ritesh <ritesh.sargam00@gmail.com>
2025-04-29 22:30:26 +0530

    Structure

---
+++ Csharp-Coding-Practice/MyStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    struct MyStruct
+    {
+        int x;
+        public MyStruct(int x)
+        {
+            this.x = x;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Method defined under a structure: " + x);
+        }
+        static void Main()
+        {
+            MyStruct m1 = new MyStruct(); m1.Display();
+            MyStruct m2; m2.x = 10; m2.Display();
+            MyStruct m3 = new MyStruct(20); m3.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/TestStruct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    struct TestStruct
+    {
+        static void Main()
+        {
+            MyStruct obj1 = new MyStruct(); obj1.Display();
+            MyStruct obj2 = new MyStruct(30); obj2.Display();
+            Console.ReadLine();
+        }
+    }
+}
