4188bb42506aa1574fb1a0ff274972c9233b5c49
Ritesh <ritesh.sargam00@gmail.com>
2025-04-30 14:30:40 +0530

    Extension Methods

---
+++ Csharp-Coding-Practice/ExtensionMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal static class ExtensionMethods
+    {
+        public static void Show(this MyStruct obj)
+        {
+            Console.WriteLine("Added this method to MyStruct structure.");
+        }
+        public static ulong Factorial(this UInt32 x)
+        {
+            if (x == 0 || x == 1)
+                return 1;
+            else if (x == 2)
+                return 2;
+            else
+                return x * Factorial(x - 1);
+        }
+        public static string ToPascal(this String OldStr)
+        {
+            if (OldStr.Trim().Length > 0)
+            {
+                OldStr = OldStr.ToLower();
+                string[] sarr = OldStr.Split(' ');
+                String NewStr = null;
+                foreach (string str in sarr)
+                {
+                    char[] carr = str.ToCharArray();
+                    carr[0] = Char.ToUpper(carr[0]);
+                    if (NewStr == null)
+                    {
+                        NewStr = new String(carr);
+                    }
+                    else
+                    {
+                        NewStr += " " + new String(carr);
+                    }
+                }
+                return NewStr;
+            }
+            return OldStr;
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/TestExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class TestExtensionMethods
+    {
+        static void Main()
+        {
+
+            MyStruct m = new MyStruct();
+            m.Show();
+
+            uint i = 8;
+            ulong result = i.Factorial();
+            Console.WriteLine($"Factorial of {i} is: {result}");
+
+            string str = "heLlO hoW aRe YoU";
+            str = str.ToPascal();
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
