28e8ce2fe53f22b3f65ab6085466278daec6ce8a
Ritesh <ritesh.sargam00@gmail.com>
2025-05-01 23:28:59 +0530

    Properties

---
+++ Csharp-Coding-Practice/Customer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class Customer
+    {
+        int _Custid;
+        bool _Status;
+        string _Name, _State;
+        double _Balance;
+        Cities _City;
+        public Customer(int Custid)
+        {
+            _Custid = Custid;
+            _Status = false;
+            _Name = "John";
+            _Balance = 5000.00;
+            _City = 0;
+            _State = "Karnataka";
+            Country = "India";
+        }
+        //Read Only Property
+        public int Custid
+        {
+            get { return _Custid; }
+        }
+        //Read-Write Property
+        public bool Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+        //Read-Write Property (With a condition in Set Accessor)
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Status)
+                {
+                    _Name = value;
+                }
+            }
+        }
+        //Read-Write Property (With a condition in Get & Set Accessor)
+        public double Balance
+        {
+            get
+            {
+                if (_Status)
+                {
+                    return _Balance;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                if (_Status)
+                {
+                    if (value >= 500)
+                    {
+                        _Balance = value;
+                    }
+                }
+            }
+        }
+        //Read-Write Property (Enumerated Property)
+        public Cities City
+        {
+            get { return _City; }
+            set
+            {
+                if (_Status)
+                {
+                    _City = value;
+                }
+            }
+        }
+        //Read-Write Property (With a different scope to each property accessor (C# 2.0))
+        public string State
+        {
+            get { return _State; }
+            protected set
+            {
+                if (_Status)
+                {
+                    _State = value;
+                }
+            }
+        }
+        //Read-Write Property (Automatic or Auto-Implemented property (C# 3.0))
+        public string Country
+        {
+            get;
+            private set;
+        }
+        //Read-Write Property (Auto property initializer (C# 6.0))
+        public string Continent { get; } = "Asia";
+    }
+}
---
+++ Csharp-Coding-Practice/TestCustomer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class TestCustomer
+    {
+        static void Main()
+        {
+            Customer obj = new Customer(1001);
+            Console.WriteLine("Custid: " + obj.Custid + "\n");
+            //obj.Custid = 1005;  //Invalid, because the property is defined read only
+
+            if (obj.Status)
+                Console.WriteLine("Customer Status: Active");
+            else
+                Console.WriteLine("Customer Status: In-Active");
+            Console.WriteLine("Customer Name: " + obj.Name);
+            obj.Name += " Smith"; //Update fails because status is in-active
+            Console.WriteLine("Name when update failed: " + obj.Name);
+            Console.WriteLine("Balance when status is in-active: " + obj.Balance + "\n");
+
+            obj.Status = true; //Activating the status
+            if (obj.Status)
+                Console.WriteLine("Customer Status: Active");
+            else
+                Console.WriteLine("Customer Status: In-Active");
+            Console.WriteLine("Customer Name: " + obj.Name);
+            obj.Name += " Smith"; //Update succeds because status is in-active
+            Console.WriteLine("Name when update succeded: " + obj.Name);
+            Console.WriteLine("Balance when status is active: " + obj.Balance + "\n");
+
+            obj.Balance -= 4600; //Transaction failed
+            Console.WriteLine("Balance when transaction failed: " + obj.Balance);
+            obj.Balance -= 4500; //Transaction succeds
+            Console.WriteLine("Balance when transaction succeded: " + obj.Balance + "\n");
+
+            Console.WriteLine("Current City: " + obj.City);
+            obj.City = Cities.Hyderabad;
+            Console.WriteLine("Modified City: " + obj.City);
+
+            Console.WriteLine("Customer State: " + obj.State);
+            //obj.State = "Telangana"; //Invalid because set accessor is accessible only to child classes
+
+            Console.WriteLine("Customer Country: " + obj.Country);
+            Console.WriteLine("Customer Continent: " + obj.Continent);
+            Console.ReadLine();
+        }
+    }
+}
