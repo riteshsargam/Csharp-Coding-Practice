045448dceff01d11e22cd4c4f4823fd191d5b4c4
Ritesh <ritesh.sargam00@gmail.com>
2025-05-05 13:15:04 +0530

    Exception Handling

---
+++ Csharp-Coding-Practice/TryCatchDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class TryCatchDemo
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter 1st number: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Enter 2nd number: ");
+                int y = int.Parse(Console.ReadLine());
+                int z = x / y;
+                Console.WriteLine("The result of division is: " + z);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Value of divisor can't be zero.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Input values must  be integers.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("End of the Program.");
+        }
+    }
+}
