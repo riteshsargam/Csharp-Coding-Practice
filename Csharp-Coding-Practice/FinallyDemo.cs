05c3fe5874a33bf6a935a808b8fea35172e2eafd
Ritesh <ritesh.sargam00@gmail.com>
2025-05-06 10:18:44 +0530

    Finally Block

---
+++ Csharp-Coding-Practice/FinallyDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class FinallyDemo
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter 1st number: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Enter 2nd number: ");
+                int y = int.Parse(Console.ReadLine());
+                if (y == 1)
+                {
+                    return;
+                }
+                int z = x / y;
+                Console.WriteLine("The result of division is: " + z);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Finally block got executed.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("End of the Program.");
+        }
+    }
+}
