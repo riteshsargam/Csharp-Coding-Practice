cfd137019b2dd768799ed87dde6dc48be87f5e99
Ritesh <ritesh.sargam00@gmail.com>
2025-05-07 14:07:56 +0530

    Application Exceptions

---
+++ Csharp-Coding-Practice/DivideByOddNoException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public class DivideByOddNoException : ApplicationException
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Attempted to divide by odd number.";
+            }
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/ThrowDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class ThrowDemo
+    {
+        static void Main()
+        {
+            Console.Write("Enter 1st number: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter 2nd number: ");
+            int y = int.Parse(Console.ReadLine());
+            if (y % 2 > 0)
+            {
+                throw new ApplicationException("Divisor can't be an odd number.");
+                //throw new DivideByOddNoException();     
+            }
+            int z = x / y;
+            Console.WriteLine("The result of division is: " + z);
+            Console.WriteLine("End of the Program.");
+        }
+    }
+}
