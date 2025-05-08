00e335b316af1e50cf095c504ee111e4d3f65298
Ritesh <ritesh.sargam00@gmail.com>
2025-05-08 20:31:47 +0530

    Delegates

---
+++ Csharp-Coding-Practice/DelDemo1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class DelDemo1
+    {
+        public void AddNums(int x, int y, int z)
+        {
+            Console.WriteLine($"Sum of given 3 no's is: {x + y + z}");
+        }
+        public static string SayHello(string Name)
+        {
+            return $"Hello {Name}, have a nice day!";
+        }
+        static void Main()
+        {
+            DelDemo1 obj = new DelDemo1();
+
+            CalculatorDelegate cd = obj.AddNums;
+            cd(10, 20, 30); cd(40, 50, 60); cd(70, 80, 90);
+
+            WishDelegate wd = DelDemo1.SayHello;
+            Console.WriteLine(wd("Raju"));
+            Console.WriteLine(wd("Vijay"));
+            Console.WriteLine(wd("Naresh"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/Delegates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    public delegate void MathDelegate(int x, int y);
+    public delegate string WishDelegate(string str);
+    public delegate void CalculatorDelegate(int a, int b, int c);
+}
+
