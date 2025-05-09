5d05e969d2cee902f741592fb3422445c9c00e02
Ritesh <ritesh.sargam00@gmail.com>
2025-05-09 19:42:55 +0530

    Multicast Delegate

---
+++ Csharp-Coding-Practice/DelDemo2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class DelDemo2
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine($"Add: {x + y}");
+        }
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine($"Sub: {x - y}");
+        }
+        public void Mul(int x, int y)
+        {
+            Console.WriteLine($"Mul: {x * y}");
+        }
+        public void Div(int x, int y)
+        {
+            Console.WriteLine($"Div: {x / y}");
+        }
+        static void Main()
+        {
+            DelDemo2 obj = new DelDemo2();
+            MathDelegate md = obj.Add;
+            md += obj.Sub; md += obj.Mul; md += obj.Div;
+
+            md(100, 25);
+            Console.WriteLine();
+            md(760, 20);
+            Console.WriteLine();
+            md -= obj.Mul;
+            md(930, 15);
+            Console.ReadLine();
+        }
+    }
+}
