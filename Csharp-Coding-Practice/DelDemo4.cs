ed23b47acba45ec7c33af48e842dfa8560cd7970
Ritesh <ritesh.sargam00@gmail.com>
2025-05-11 13:59:44 +0530

    Lambda Expression

---
+++ Csharp-Coding-Practice/DelDemo4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class DelDemo4
+    {
+        static void Main()
+        {
+            CalculatorDelegate cd = (a, b, c) =>
+            {
+                Console.WriteLine($"Product of given numbers: {a * b * c}");
+            };
+            cd(10, 20, 30);
+            cd(40, 50, 60);
+            cd(70, 80, 90);
+
+            WishDelegate wd = user =>
+            {
+                return $"Hello {user}, welcome to the application.";
+            };
+            Console.WriteLine(wd("Raju"));
+            Console.WriteLine(wd("Pooja"));
+            Console.WriteLine(wd("Praveen"));
+            Console.ReadLine();
+        }
+    }
+}
