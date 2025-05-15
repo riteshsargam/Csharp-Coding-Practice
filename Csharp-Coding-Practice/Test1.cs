b579b9fef3d441f3423d3fd38447fd10955d72d4
Ritesh <ritesh.sargam00@gmail.com>
2025-05-15 20:27:47 +0530

    Partial Methods

---
+++ Csharp-Coding-Practice/Test1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    partial class Test
+    {
+        partial void Method1();         //Partial method declaration in Test1.cs file
+        public void Method2()
+        {
+            Console.WriteLine("Method 2.");
+            Method1();          //Calling the Partial Method declared above
+        }
+    }
+}
---
+++ Csharp-Coding-Practice/Test2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    partial class Test
+    {
+        partial void Method1()  //Implementing the Partial Method declared in Test1.cs under Test2.cs
+        {
+            Console.WriteLine("Method 1.");
+        }
+        static void Main()
+        {
+            Test obj = new Test();
+            obj.Method2; Console.ReadLine();
+        }
+    }
+}
