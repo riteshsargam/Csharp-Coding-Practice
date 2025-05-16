4134ff2d827cb14d206e66af668739e23c67ae95
Ritesh <ritesh.sargam00@gmail.com>
2025-05-16 20:46:16 +0530

    Queue (FIFO)

---
+++ Csharp-Coding-Practice/QueueDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace Csharp_Coding_Practice
+{
+    internal class QueueDemo
+    {
+        static void Main()
+        {
+            Queue q = new Queue();
+
+            q.Enqueue('A');
+            q.Enqueue(100);
+            q.Enqueue(false);
+            q.Enqueue(34.56);
+            q.Enqueue("Hello");
+
+            foreach (object obj in q)
+            {
+                Console.Write(obj + "  ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(q.Dequeue());
+            foreach (object obj in q)
+            {
+                Console.Write(obj + "  ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
