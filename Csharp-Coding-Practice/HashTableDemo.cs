4c615d3744e7d7965afc9db57528b2a5a66eff86
Ritesh <ritesh.sargam00@gmail.com>
2025-05-18 19:24:35 +0530

    HashTable

---
+++ Csharp-Coding-Practice/HashTableDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class HashTableDemo
+    {
+        static void Main()
+        {
+            Hashtable Emp = new Hashtable();
+            Emp.Add("Emp-Id", 1001);
+            Emp.Add("Emp-Name", "Scott");
+            Emp.Add("Job", "CEO");
+            Emp.Add("Mgr-Id", null);
+            Emp.Add("Salary", 50000.00);
+            Emp.Add("Commission", 0.00f);
+            Emp.Add("Dept-Id", 10);
+            Emp.Add("Dept-Name", "Administration");
+            Emp.Add("Location", "Mumbai");
+            Emp.Add("Status", true);
+            Emp.Add("PAN", "AKYPM 1234K");
+            Emp.Add("Adhar No.", "1234 5678 9012");
+            Emp.Add("Mobile", "98392 14256");
+            Emp.Add("Home Phone", "2718 6547");
+            Emp.Add("Email", "Scott@gmail.com");
+
+            foreach (object key in Emp.Keys)
+            {
+                Console.WriteLine($"{key}: {Emp[key]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
