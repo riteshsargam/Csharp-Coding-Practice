8ae049474fe0a94120bbfbc6543a888ed860769f
Ritesh <ritesh.sargam00@gmail.com>
2025-05-24 21:11:17 +0530

    Collection Initializers

---
+++ Csharp-Coding-Practice/CollectionInitializers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class CollectionInitializers
+    {
+        static void Main()
+        {
+            //Copying values > 40 from 1 list to another list and arranging them in descending order
+            List<int> coll1 = new List<int>() { 13, 56, 29, 98, 24, 54, 79, 39, 8, 42, 22, 93, 6, 73, 35, 67, 48, 18, 61, 32, 86, 15, 21, 81, 2 };
+            List<int> coll2 = new List<int>();
+
+            foreach (int i in coll1)
+            {
+                if (i > 40)
+                {
+                    coll2.Add(i);
+                }
+            }
+            coll2.Sort();
+            coll2.Reverse();
+            Console.WriteLine(String.Join(", ", coll2));
+            Console.ReadLine();
+        }
+    }
+}
