a0532e2b2cf88da1c307b4b3d5fe2fb12fbcacfd
Ritesh <ritesh.sargam00@gmail.com>
2025-05-27 14:33:11 +0530

    Task Parallel Library

---
+++ Csharp-Coding-Practice/TaskParallelLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class TaskParallelLibrary
+    {
+        static void Print1()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print1 Method: {i}");
+            }
+        }
+        static void Print2()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print2 Method: {i}");
+            }
+        }
+        static void Print3()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print3 Method: {i}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Print1(); Print2(); Print3();
+        }
+    }
+}
