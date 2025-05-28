ff792b08f7eb23b6642e1030114c6152427ec17d
Ritesh <ritesh.sargam00@gmail.com>
2025-05-28 16:40:26 +0530

    TPL Multi Threading

---
+++ Csharp-Coding-Practice/TPLMultiThreading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class TPLMultiThreading
+    {
+        static void Print1()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print1 Method: {i}");
+            }
+        }
+        static void Print2()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print2 Method: {i}");
+            }
+        }
+        static void Print3()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print3 Method: {i}");
+            }
+        }
+        static void Main()
+        {
+            Thread t1 = new Thread(Print1);
+            Thread t2 = new Thread(Print2);
+            Thread t3 = new Thread(Print3);
+            t1.Start(); t2.Start(); t3.Start();
+            t1.Join(); t2.Join(); t3.Join();
+            Console.WriteLine($"Main thread with Id: {Thread.CurrentThread.ManagedThreadId} is exiting.");
+        }
+    }
+}
