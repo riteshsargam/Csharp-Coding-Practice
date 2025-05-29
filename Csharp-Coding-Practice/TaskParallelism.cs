14954df7bc42a936d06bc5e27d674e0122a029be
Ritesh <ritesh.sargam00@gmail.com>
2025-05-29 10:49:08 +0530

    Task Parallelism

---
+++ Csharp-Coding-Practice/TaskParallelism.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class TaskParallelism
+    {
+        static void Print1()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print1 Method: {i}");
+            }
+        }
+        static void Print2()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print2 Method: {i}");
+            }
+        }
+        static void Print3()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print3 Method: {i}");
+            }
+        }
+        static void Main()
+        {
+            Task t1 = new Task(Print1);
+            Task t2 = new Task(Print2);
+            Task t3 = new Task(Print3);
+            t1.Start(); t2.Start(); t3.Start();
+            t1.Wait(); t2.Wait(); t3.Wait();
+            Console.WriteLine($"Main thread with Id: {Thread.CurrentThread.ManagedThreadId} is exiting.");
+        }
+    }
+}
