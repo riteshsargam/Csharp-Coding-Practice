7c1113e90e4c489de693af7aeaf0a58a6c3a5f65
Ritesh <ritesh.sargam00@gmail.com>
2025-06-01 11:29:28 +0530

    Thread Synchronization

---
+++ Csharp-Coding-Practice/ThreadSynchronization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class ThreadSynchronization
+    {
+        public static void Print()
+        {
+            lock (typeof(ThreadSynchronization))
+            {
+                Console.Write("[CSharp Is ");
+                Thread.Sleep(5000);
+                Console.WriteLine("Object Oriented]");
+            }
+        }
+        static void Main()
+        {
+            Thread t1 = new Thread(Print);
+            Thread t2 = new Thread(Print);
+            Thread t3 = new Thread(Print);
+            t1.Start(); t2.Start(); t3.Start();
+            t1.Join(); t2.Join(); t3.Join();
+        }
+    }
+}
