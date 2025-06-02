11ed93e56f5ad0124153db00a338ef96c6453f7d
Ritesh <ritesh.sargam00@gmail.com>
2025-06-02 10:44:52 +0530

    Asynchronous programming

---
+++ Csharp-Coding-Practice/Async.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Coding_Practice
+{
+    internal class Async
+    {
+        static async void Test1()
+        {
+            Console.WriteLine("Started reading values from DB.....");
+            await Task.Delay(10000);
+            Console.WriteLine("Completed reading values from DB.....");
+        }
+        static void Test2()
+        {
+            Console.Write("Please enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Name you entered is: {Name}");
+        }
+        static void Main()
+        {
+            Test1();
+            Test2();
+            Console.ReadLine();
+        }
+    }
+}
